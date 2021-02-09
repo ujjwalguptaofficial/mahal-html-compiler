a2d22b13fb60559ec7093def986b9338972cabc6
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2021-02-09 09:55:23 +0530

    add simple test

--- test/simple.test.ts
+++ test/simple.test.ts
@@ -4,4 +4,35 @@ describe('simple', function () {
     it('div with mustache expression', () => {
         createRenderer(`<div>{{ujjwal}}</div>`)
     })
+
+    it('div with simple inner text', () => {
+        createRenderer(`<div>ujjwal</div>`)
+    })
+
+    it('div with unclosed tag', (done) => {
+        new Promise(() => {
+            createRenderer(`<div>`)
+        }).catch(() => {
+            done();
+        });
+    })
+
+    it('div with self closing tag', () => {
+        createRenderer(`<div/>`);
+    })
+
+    it('multiple parent', (done) => {
+        new Promise(() => {
+            createRenderer(`<div></div><div></div>`);
+
+        }).catch(() => {
+            done();
+        });
+    })
+
+    it('multiline', () => {
+        createRenderer(`<div>
+          <div>Ujjwal</div>
+        </div>`);
+    })
 })
