999637ac9d792805e9fd793cd75729d2da1114b1
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2021-02-09 10:05:14 +0530

    add event test

---
+++ test/event.test.ts
@@ -0,0 +1,22 @@
+import { createRenderer } from "../src/index";
+
+describe('simple', function () {
+    it('on:click on div', () => {
+        createRenderer(`<div on:click="handleClick">Click me</div>`)
+    })
+
+    it('on:click on btn inside div', () => {
+        createRenderer(`<div>
+            <btn on:click="handleClick">Click me</btn>
+        </div>`)
+    })
+
+    it('on:click on btn inside div', () => {
+        createRenderer(`
+    <div>
+        <div id="counter">{{counter}}</div>
+        <button id="btnIncrement" on:click='increment'>Increment</button>
+        <button id="btnDecrement" on:click='decrement'>Decrement</button>
+    </div>`)
+    })
+})
