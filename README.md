5b6cb9039ed2c42fa54c8b39fe7bfeb2b3952545
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2021-02-21 11:14:55 +0530

    add license

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# taj-html-compiler
+# mahal-html-compiler
+
+
+
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
+const SmartBannerPlugin = require('smart-banner-webpack-plugin');
+const banner = require('../build_helper/license');
 
 module.exports = {
-    entry: path.resolve(__dirname, 'src/index.ts'),
+    entry: path.resolve(__dirname, '../src/index.ts'),
     devtool: 'source-map',
     target: "node",
-
     mode: "development",
     module: {
         rules: [{
@@ -20,11 +21,11 @@ module.exports = {
     output: {
         filename: 'compiler.js',
         path: path.resolve(process.cwd(), 'dist/'),
-        library: 'tajHtmlCompiler',
+        library: 'mahalHtmlCompiler',
         libraryTarget: "commonjs2"
     },
     plugins: [
-
+        new SmartBannerPlugin(banner),
     ],
     externals: [nodeExternals()]
 };
