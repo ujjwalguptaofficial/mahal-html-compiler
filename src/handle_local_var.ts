15753a5c32d5f21ae54351eb3ea0f7be992dfeb4
Ujjwal Gupta <ujjwalkumargupta4u@gmail.com>
2022-05-04 15:55:48 +0530

    fix: issue with variable which contains similar name in case of local vars

--- src/handle_local_var.ts
+++ src/handle_local_var.ts
@@ -1,11 +1,19 @@
 import { IExpression } from "./add_ctx_to_expression";
 import { CONTEXT_STRING } from "./constant";
 
+const alphabetRegex = /[a-z]|[A-Z]|[_]/;
+
 export const handleLocalVar = (localVars: string[], exp: IExpression) => {
     (localVars || []).forEach(localVar => {
         const expressionToCheck = CONTEXT_STRING + "." + localVar;
-        if (exp.expStr.includes(expressionToCheck)) {
-            exp.expStr = exp.expStr.replace(expressionToCheck, localVar);
+        const expStr = exp.expStr;
+        const index = expStr.indexOf(expressionToCheck)
+        if (index >= 0) {
+            const nextChar = expStr[index + expressionToCheck.length];
+            if (nextChar && nextChar.match(alphabetRegex)) {
+                return;
+            }
+            exp.expStr = expStr.replace(expressionToCheck, localVar);
             const indexOfKey = exp.keys.findIndex(q => q === localVar);
             exp.keys.splice(indexOfKey, 1);
         }
